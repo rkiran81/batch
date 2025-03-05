7041bf9e3fba73c1f08ae35e40fa5e107fe7025e
Kiran Kumar <kiranr.kumar@gmail.com>
2025-03-05 18:54:44 +0530

    111

--- 04/demos/angular-microfrontend-example/projects/shared/src/lib/product.ts
+++
@@ -1,7 +0,0 @@
-export interface Product {
-    id: number;
-    name: string;
-    link: string;
-    description: string;
-}
-
--- 04/demos/angular-monorepo/libs/shared/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of shared
- */
-
-export * from './lib/products.service';
-export * from './lib/product';
-export * from './lib/basket.service';
